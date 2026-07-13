b2a78a84f0309d69c408abc673886d086fd69f4c
dianro0110-prog <dianro0110@gmail.com>
2026-07-13 23:33:32 +0700

    Fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: "30px" }}>
-      <h1>WMS Logistik</h1>
-      <p>Dashboard Warehouse Management System</p>
-    </div>
-  );
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/login");
 }
